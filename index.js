3339706e28f4a467abbae93c1494ebc21b8fbf1c
HansHuang <hys0550@sina.com>
2025-04-12 17:56:01 -0400

    add api refresh

--- index.js
+++ index.js
@@ -7,19 +7,13 @@ const fs = require('fs'),
 //TODO: watch file changed for json template
 
 module.exports = ({ urlPrefix, filePath, dummyOptions }) => {
-    const source = fs.readFileSync(filePath).toString()
-    let jsonData = {}
-    if (path.extname(filePath).toLowerCase() == '.json') {
-        jsonData = JSON.parse(source)
-    } else {
-        let jsonResult = dummyJson.parse(source, dummyOptions)
-        jsonData = JSON.parse(jsonResult)
-    }
 
     const router = new Router()
     urlPrefix && router.prefix(urlPrefix)
+    const jsonData = generateData(filePath, dummyOptions)
 
-    router.get('/:name{/:id}', require('./lib/get')(jsonData))
+    router.get('/_refresh', handleRefresh(jsonData))
+        .get('/:name{/:id}', require('./lib/get')(jsonData))
         .delete('/:name{/:id}', require('./lib/delete')(jsonData))
         .all('{/:name}', handleBadRequest)
         .post('{/:name}', require('./lib/create')(jsonData))
@@ -29,9 +23,17 @@ module.exports = ({ urlPrefix, filePath, dummyOptions }) => {
     return router.routes()
 }
 
+function handleRefresh(jsonData) {
+    return async (ctx) => {
+        const newData = generateData(filePath, dummyOptions);
+        Object.keys(jsonData).forEach(key => delete jsonData[key]);
+        Object.assign(jsonData, newData);
+
+        ctx.body = { message: 'Data refreshed' }
+    }
+}
 
 async function handleBadRequest(ctx, next) {
-
     if (['POST', 'PUT', 'PATCH'].includes(ctx.method)) {
         let entity = ctx.request.body,
             entityType = typeof entity
@@ -43,5 +45,17 @@ async function handleBadRequest(ctx, next) {
     }
 
     return await next()
+}
+
+function generateData(filePath, dummyOptions) {
+    const source = fs.readFileSync(filePath).toString()
+    let jsonData = {}
+    if (path.extname(filePath).toLowerCase() == '.json') {
+        jsonData = JSON.parse(source)
+    } else {
+        let jsonResult = dummyJson.parse(source, dummyOptions)
+        jsonData = JSON.parse(jsonResult)
+    }
+    return jsonData
 }
 
